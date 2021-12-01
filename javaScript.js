626d1ad57286ee4913fc146d6429eb8bed714208
SkirnevskyiAleksandr <sanya-sha2016@ukr.net>
2021-12-01 23:32:52 +0200

    feat

--- javaScript.js
+++ javaScript.js
@@ -1,41 +1,63 @@
 
 
-// get all tag-form
+// // get all tag-form
+// const forms = document.querySelectorAll("form")
+
+// //iterate psevdo-masive forms
+// forms.forEach((item) => {
+//     formWrap(item)
+// })
+
+// function formWrap(form) {
+//     form.addEventListener('submit', (e) => {
+//         // get rid of default events
+//         e.preventDefault()
+//         const formData = new FormData(form) //create FormData object
+
+//         const request = new XMLHttpRequest() //create XMLHt...-object
+//         request.open('POST', "./js/server.php")
+//         // request.setRequestHeader("Content-type") - //not use with FormData+XMLHttpRequest
+//         request.send(formData)
+
+//         request.addEventListener('load', () => {
+//             console.log(request.response)
+//         })
+
+
+//     })
+// }
+
+
+// //FETCH - Request(example)
+// fetch("https://jsonplaceholder.typicode.com/posts",
+
+//     {
+//         method: "POST",
+//         body: JSON.stringify({ name: 'Alex' }),
+//         headers: { "Content-type": "application/json" }
+//     }
+// )
+//     .then(response => response.json())
+//     .then(json => console.log(json))
+
+
+
 const forms = document.querySelectorAll("form")
 
-//iterate psevdo-masive forms
-forms.forEach((item) => {
-    formWrap(item)
-})
 
-function formWrap(form) {
+function sendForms(form) {
+    const formDate = new FormData(form)
     form.addEventListener('submit', (e) => {
-        // get rid of default events
         e.preventDefault()
-        const formData = new FormData(form) //create FormData object
-
-        const request = new XMLHttpRequest() //create XMLHt...-object
-        request.open('POST', "./js/server.php")
-        // request.setRequestHeader("Content-type") - //not use with FormData+XMLHttpRequest
-        request.send(formData)
-
-        request.addEventListener('load', () => {
-            console.log(request.response)
-        })
-
+        fentch('./js/server.php',
+            {
+                method: "POST",
+                body: formDate,
+                headers: { "Content-type": application / json }
+            })
+            .then(response => response.json())
+            .then(data => console.log(data))
 
     })
 }
-
-
-//FETCH - Request(example)
-fetch("https://jsonplaceholder.typicode.com/posts",
-
-    {
-        method: "POST",
-        body: JSON.stringify({ name: 'Alex' }),
-        headers: { "Content-type": "application/json" }
-    }
-)
-    .then(response => response.json())
-    .then(json => console.log(json))
+forms.forEach(item => sendForms(item))
