f15ad31b4be6f4b648d23dd60bfdc653fb62fe7b
SkirnevskyiAleksandr <sanya-sha2016@ukr.net>
2021-12-01 23:33:36 +0200

    feat:chamge

--- javaScript.js
+++ javaScript.js
@@ -1,63 +1,44 @@
 
 
-// // get all tag-form
-// const forms = document.querySelectorAll("form")
-
-// //iterate psevdo-masive forms
-// forms.forEach((item) => {
-//     formWrap(item)
-// })
-
-// function formWrap(form) {
-//     form.addEventListener('submit', (e) => {
-//         // get rid of default events
-//         e.preventDefault()
-//         const formData = new FormData(form) //create FormData object
-
-//         const request = new XMLHttpRequest() //create XMLHt...-object
-//         request.open('POST', "./js/server.php")
-//         // request.setRequestHeader("Content-type") - //not use with FormData+XMLHttpRequest
-//         request.send(formData)
-
-//         request.addEventListener('load', () => {
-//             console.log(request.response)
-//         })
-
-
-//     })
-// }
-
-
-// //FETCH - Request(example)
-// fetch("https://jsonplaceholder.typicode.com/posts",
-
-//     {
-//         method: "POST",
-//         body: JSON.stringify({ name: 'Alex' }),
-//         headers: { "Content-type": "application/json" }
-//     }
-// )
-//     .then(response => response.json())
-//     .then(json => console.log(json))
-
-
-
+// get all tag-form
 const forms = document.querySelectorAll("form")
 
+//iterate psevdo-masive forms
+forms.forEach((item) => {
+    formWrap(item)
+})
 
-function sendForms(form) {
-    const formDate = new FormData(form)
+function formWrap(form) {
     form.addEventListener('submit', (e) => {
+        // get rid of default events
         e.preventDefault()
-        fentch('./js/server.php',
-            {
-                method: "POST",
-                body: formDate,
-                headers: { "Content-type": application / json }
-            })
-            .then(response => response.json())
-            .then(data => console.log(data))
+        const formData = new FormData(form) //create FormData object
+
+        const request = new XMLHttpRequest() //create XMLHt...-object
+        request.open('POST', "./js/server.php")
+        // request.setRequestHeader("Content-type") - //not use with FormData+XMLHttpRequest
+        request.send(formData)
+
+        request.addEventListener('load', () => {
+            console.log(request.response)
+        })
+
 
     })
 }
-forms.forEach(item => sendForms(item))
+
+
+//FETCH - Request(example)
+fetch("https://jsonplaceholder.typicode.com/posts",
+
+    {
+        method: "POST",
+        body: JSON.stringify({ name: 'Alex' }),
+        headers: { "Content-type": "application/json" }
+    }
+)
+    .then(response => response.json())
+    .then(json => console.log(json))
+
+
+
